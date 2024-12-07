ebbf18e5a9dbeceb8d8278cda9176892718b1a95
PiyushGhavghave <piyushghavghave@gmail.com>
2024-12-08 00:18:19 +0530

    Fixed import

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import Header from '../components/Header'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar'
 import { Button } from '../components/ui/button'
 import { Menu } from 'lucide-react'
 
